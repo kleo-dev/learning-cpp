2e01c532d8b31b1b596e2c5fcdf82ba563ca7812
Leo Dev <klesti.selimay@icloud.com>
2024-02-03 00:28:00 +0100

    data types

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <ostream>
 #include <stdio.h>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
 
 int main() {
-  char input[3];
-  std::cin >> input;
-  std::cout << input;
+  string input;
+  cin >> input;
+  string *ptr = &input;
+  string x = *ptr;
+  x = "test";
+  cout << x << endl << input << endl;
+  // arrays
+  string my_array[5];
+  my_array->append("XYZ");
+  cout << my_array[0] << endl;
+  // hashMaps
+  unordered_map<string, string> my_map;
+  my_map.insert({"test", "abc"});
   return 0;
 }
