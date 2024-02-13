2c6341ed18d4fb8b425795c13d809567a550ca87
Leo Dev <klesti.selimay@icloud.com>
2024-02-13 16:09:48 +0100

    New learning from book

--- main.cpp
+++ main.cpp
@@ -1,24 +1,47 @@
+#include <cstdio>
 #include <iostream>
-#include <ostream>
-#include <stdio.h>
-#include <string>
-#include <unordered_map>
 
-using namespace std;
+// using std::cin;
+using std::cout;
+using std::endl;
+
+enum myEnum { x, y, z };
+
+class MyClass {
+public:
+  char x = 'x';
+  int y = 10;
+};
 
 int main() {
-  string input;
-  cin >> input;
-  string *ptr = &input;
-  string x = *ptr;
-  x = "test";
-  cout << x << endl << input << endl;
-  // arrays
-  string my_array[5];
-  my_array->append("XYZ");
-  cout << my_array[0] << endl;
-  // hashMaps
-  unordered_map<string, string> my_map;
-  my_map.insert({"test", "abc"});
+
+  MyClass myObject;
+  printf("the MyClass values are: (x=%c y=%i)\n", myObject.x, myObject.y);
+
+  int brinjaA, brinjaB, Syprina, Perimetri;
+  brinjaA = 5;
+  brinjaB = 10;
+  Syprina = brinjaA * brinjaB;
+  Perimetri = (2 * brinjaA) + (2 * brinjaB);
+
+  cout << "Syprina: " << Syprina << ", Perimetri: " << Perimetri;
+  cout << "test" << endl;
+
+  // enum
+  myEnum testEnumValue;
+  testEnumValue = y;
+  cout << "values inside enum: " << testEnumValue << endl;
+
+  // switch
+
+  switch (testEnumValue) {
+  case x:
+    cout << "the Enum value is x" << endl;
+  case y:
+    cout << "the Enum value is y" << endl;
+  default:
+    break;
+  }
+
   return 0;
 }
